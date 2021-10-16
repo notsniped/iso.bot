5259143b883a56f63b970792868b27af940b49e1
SniperBlaze <72265661+shoddyDEV@users.noreply.github.com>
2021-10-16 12:46:37 +0530

    Create runtime-engine.js

---
+++ subpage/subassets/runtime-engine.js
@@ -0,0 +1,19 @@
+// Isobot runtime-engine.js
+// Version 1.0
+// API Version 1.0.1
+// Developed by notsniped#6776 for iso.bot and isobot#6851
+// Copyright 2021
+
+// Def begin //
+
+function invite_iso_main() {
+    window.location.replace("https://discord.com/api/oauth2/authorize?client_id=896437848176230411&permissions=8&scope=bot")
+}
+function invite_iso_limited() {
+    window.location.replace("https://discord.com/api/oauth2/authorize?client_id=896437848176230411&permissions=536807996657&scope=bot")
+}
+function invite_iso_minimalist() {
+    window.location.replace("https://discord.com/api/oauth2/authorize?client_id=896437848176230411&permissions=519628119281&scope=bot")
+}
+
+// Def end //
